45fef85d5c1bfe496e44fc5d76c35e62527982b5
Guzmansrz <guzmansuarezalvarez@gmail.com>
2022-02-07 12:51:29 +0100

    inicio de las pruebas

---
+++ Prueba javascript/script.js
@@ -0,0 +1,13 @@
+var mi_variable = 2;
+console.log("Hola mundo");
+console.log(mi_variable);
+console.log("Hola desde otro archivo");
+
+var n1 = 4;
+var n2 = 4;
+
+if (n1 > n2) {
+  console.log("n1 es mayor");
+} else {
+  console.log("n1 es menor");
+}
